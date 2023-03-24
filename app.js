cb3ad729c1d316012db8c1357dcfd89a1684b4fd
Nakuljod <113530964+Nakuljod@users.noreply.github.com>
2023-03-24 13:32:21 +0530

    functions

--- app.js
+++ app.js
@@ -1,7 +1,96 @@
 var n = 2;
+debugger
 function square (num) {
     var ans = num * num;
     return ans;
 }
 var square2 = square(n);
 var square4 = square(4);
+
+
+// var x = 7;
+
+// function getName() {
+//     console.log("NAMSTE JAVASCRIPT")
+// }
+// getName();
+// console.log(x);
+
+// undefined and not defined
+
+// var a;
+// console.log(a);
+// a = 10;
+// console.log(a);
+// a = "hello world";
+// console.log(a);
+
+// undefined meaning tha variable has assigned the memory
+// while in not defined the memory is not assigned
+
+
+
+// scope and lexiacal env
+// function a() {
+//     var b = 10;
+//     caches();
+//     function c(){
+
+//     }
+// }
+// a():
+// console.log(b);
+
+
+// lexical env is invoked when execution context is invoked
+// it is a structure where var stores along with function declaration
+// reconginse which varible is in which scope
+
+
+
+// let and cons
+
+
+// let a = 10;
+// console.log(a);
+// var b = 100;
+// let cannot be printed before intailized 
+
+
+// const should declared intailized and its value cannot change
+// let a = 100;
+// const b = 1000;
+
+// 
+// b cannot be assign value again 
+// 
+
+// a = 10;
+// console.log(a)
+// 
+
+
+
+
+
+// function statement
+
+// function a() {
+//     console.log("is called");
+// }
+
+
+// function expression 
+
+// assigning a value to function is called function expression
+
+// var b = function () {
+//     console.log("b called")
+// }
+
+// anonymous function 
+
+// function withou name 
+
+
+ 
